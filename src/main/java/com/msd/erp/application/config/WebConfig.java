44a0d9cfafbde61fcc9cbf69a62cd4b200c15964
Mădălin-Andrei Filip <filip.madalin2@gmail.com>
2024-12-26 15:07:54 +0200

    stash

--- src/main/java/com/msd/erp/application/config/WebConfig.java
+++ src/main/java/com/msd/erp/application/config/WebConfig.java
@@ -6,8 +6,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @Configuration
 public class WebConfig {
 
@@ -17,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**") 
-                        .allowedOrigins("http://localhost:5173") 
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
